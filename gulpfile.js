c2f7a53b30216ebc45afb3f27f17858c2e971bac
Mauro Aguilar <mauro_aguilar98@hotmail.com>
2017-06-26 01:29:45 -0500

    luxury theme

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,46 @@
 const path = require('path');
 const gulp = require('gulp');
 const sass = require('gulp-sass');
+const include = require('gulp-file-include');
+const rename = require('gulp-rename');
+const yargs = require('yargs');
+const fs = require('fs');
+
+const dir = (() => {
+  const dir = yargs.argv.dir;
+
+  if (!dir)
+    throw new Error('Missing [--dir] flag');
+
+  const stats = fs.statSync(path.join(__dirname, dir));
+
+  if (!stats.isDirectory())
+    throw new Error(`'${dir}' isn't a directory`);
+
+  return dir;
+})();
 
 // Tasks
-gulp.task('make-css', () => gulp.src('./**/css/main.scss', { base: './' })
+gulp.task('make-css', () => gulp.src(`./${dir}/css/main.scss`, { base: './' })
   .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
   .pipe(gulp.dest(__dirname))
 );
 
-gulp.task('sass:watch', () => gulp.watch(path.join(__dirname, '**/*.scss'), ['make-css']));
+gulp.task('merge-html', () => {
+  const src = path.join(__dirname, `${dir}/templates/layout.html`);
+  gulp.src(src)
+    .pipe(include({
+      prefix: '@@',
+      basePath: '@file'
+    }))
+    .pipe(rename(`${dir}/index.html`))
+    .pipe(gulp.dest('./'))
+});
 
-gulp.task('default', ['make-css']);
+gulp.task('watch', () => {
+  const base = path.join(__dirname, dir);
+  gulp.watch(path.join(base, 'templates/*.html'), ['merge-html']);
+  gulp.watch(path.join(base, '**/*.scss'), ['make-css']);
+});
+
+gulp.task('default', ['make-css', 'merge-html', 'watch']);
